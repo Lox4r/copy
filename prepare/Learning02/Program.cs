c743854852cc656cd09ec2a07d866e944d857027
Lox4r <loxhand@gmail.com>
2024-11-17 11:12:42 -0500

    1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create Job instances
+        Job job1 = new Job
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Microsoft",
+            _startYear = 2019,
+            _endYear = 2022
+        };
+
+        Job job2 = new Job
+        {
+            _jobTitle = "Manager",
+            _company = "Apple",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        // Create Resume instance
+        Resume myResume = new Resume
+        {
+            _name = "Allison Rose"
+        };
+
+        // Add jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display resume
+        myResume.Display();
     }
 }
