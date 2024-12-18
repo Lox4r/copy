1a2d6011bab8a071191f5a18971b4cbd54b7027f
Lox4r <loxhand@gmail.com>
2024-12-18 00:05:30 -0500

    1

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Create instances of each activity
+        Running run = new Running("03 Nov 2022", 30, 3.0);
+        Cycling bike = new Cycling("04 Nov 2022", 40, 15.0);
+        Swimming swim = new Swimming("05 Nov 2022", 25, 20);
+
+        // Add to a list and display summaries
+        List<Activity> activities = new List<Activity> { run, bike, swim };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
